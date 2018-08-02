84ee060003a96e67be3f68e15d890a55d45330d7
Sherry Ma <sherry.hyma@gmail.com>
2018-08-02 12:54:24 +0800

    add /unsubscribe and change entry format of subscribe

--- receive/index.js
+++ receive/index.js
@@ -30,16 +30,14 @@ async function run_cmd(command) {
 }
 
 bot.command('/subscribe', async(ctx, next) => {
-    ctx.reply(`Which validator would you like to subscribe?`)
-    return next(ctx).then(() => {
-        var correct = 0
+    var chatid = ctx.message.chat.id
+    var text = ctx.message.text
+    var texts = text.split(' ')
+    valaddr = texts[1]
 
-        bot.hears(/[0-9A-F]{40}/, (ctx) => {
-            // send address and chat id to mongoDB
-            // get charid and valaddr
-
-            var chatid = ctx.message.chat.id
-            var valaddr = ctx.message.text
+    if (typeof valaddr == 'undefined') {
+        ctx.reply("Usage: /subscribe [hexvaladdress]")
+    } else if (hexcosvaladdrpattern.test(valaddr)) {
 
             valaddr = valaddr.replace(/\n/, '')
             MongoClient.connect(uri, function(err, client) {
@@ -51,24 +49,71 @@ bot.command('/subscribe', async(ctx, next) => {
                 // perform actions on the collection object
                 // check if this validator address is already subscribed
                 var query = '{ "chatID": ' + chatid + ', "ValAddr": \"' + valaddr + '\" }'
-                if (collection.find(query).count() > 0) {
-                    ctx.reply("You have already subscribed this validator")
+                query = JSON.parse(query)
+                collection.find(query).toArray(function(err, result) {
+                    if (err) throw err;
+                    if (result.length > 0) {
+                        ctx.reply("You have already subscribed this validator")
+                        client.close();
+                    } else {
+                    // if not yet subscribe, add it
+                        collection.insertOne({chatID:chatid, ValAddr:valaddr, absent: false, slashed: false, revoked: false}, function(err, r) {
+                            test.equal(null, err);
+                            test.equal(1, r.insertedCount);
+                            // Finish up test
+                            client.close();
+
+                            // reply user
+                            ctx.reply("successfully subscribe " + valaddr)
+                    });
+                }
+                })
+            });
+    } else {
+        ctx.reply("Usage: /subscribe [hexvaladdress]")
+    }
+});
+
+bot.command('/unsubscribe', async(ctx, next) => {
+    var chatid = ctx.message.chat.id
+    var text = ctx.message.text
+    var texts = text.split(' ')
+    valaddr = texts[1]
+    
+    if (typeof valaddr == 'undefined') {
+        ctx.reply("Usage: /unsubscribe [hexvaladdress]")
+    } else if (hexcosvaladdrpattern.test(valaddr)) {
+
+        valaddr = valaddr.replace(/\n/, '')
+        MongoClient.connect(uri, function(err, client) {
+            if(err) {
+                console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+            }
+            console.log('Connected...');
+            const collection = client.db("GaiaBotData").collection("ValAddrID");
+            // perform actions on the collection object
+            // check if this validator address is already subscribed
+            var query = '{ "chatID": ' + chatid + ', "ValAddr": \"' + valaddr + '\" }'
+            query = JSON.parse(query)
+            collection.find(query).toArray(function(err, result) {
+                if (err) throw err;
+                if (result.length <= 0) {
+                    ctx.reply("You have not subscribed this validator yet")
                     client.close();
                 } else {
                     // if not yet subscribe, add it
-                    collection.insertOne({chatID:chatid, ValAddr:valaddr, absent: false, slashed: false, revoked: false}, function(err, r) {
-                        test.equal(null, err);
-                        test.equal(1, r.insertedCount);
+                    collection.remove(query, function(err, r) {
                         // Finish up test
                         client.close();
-
                         // reply user
-                        ctx.reply("successfully subscribe " + ctx.message.text)
+                        ctx.reply("successfully unsubscribe " + valaddr)
                     });
                 }
-            });
+            })
         })
-    })
+    } else {
+        ctx.reply("Usage: /unsubscribe [hexvaladdress]")
+    }
 });
 
 bot.command("/mute", async(ctx, next) => {
@@ -98,8 +143,6 @@ bot.command("/mute", async(ctx, next) => {
             var newvalue = '{ "$set": {\"' + type + '\": true} }'
             newvalue = JSON.parse(newvalue)
             query = JSON.parse(query)
-            // newvalue = JSON.parse(newvalue)
-            // var query =
 
             if (collection.find(query)) {
                 collection.updateOne(query, newvalue, function(err, res) {
@@ -132,7 +175,6 @@ bot.command("/unmute", async(ctx, next) => {
 
         valaddr = valaddr.replace(/\n/, '')
 
-        // if (cosvaladdrpattern.test(cosvaladdr)) {
         // if validatoraddr exists in the db, change mute to true
         MongoClient.connect(uri, function(err, client) {
             if(err) {
@@ -145,8 +187,6 @@ bot.command("/unmute", async(ctx, next) => {
             var newvalue = '{ "$set": {\"' + type + '\": false} }'
             newvalue = JSON.parse(newvalue)
             query = JSON.parse(query)
-            // newvalue = JSON.parse(newvalue)
-            // var query =
 
             if (collection.find(query)) {
                 collection.updateOne(query, newvalue, function(err, res) {
